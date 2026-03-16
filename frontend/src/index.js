7aae532a5de10791421810e203afb551a13f2c50
muhammed1675 <ayoolamuhammed05@gmail.com>
2026-03-16 10:20:21 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,3 +5,12 @@ import App from "@/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
+
+            if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then((reg) => console.log('SW registered:', reg.scope))
+      .catch((err) => console.error('SW registration failed:', err));
+  });
+}
